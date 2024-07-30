31bc66d676884dfc61b0c62b86580b89c05dbe0c
git-hub-boss <ddosebot@gmail.com>
2024-07-30 21:11:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7215867283:AAH399xIuKfLFofx5ThloVTWqydSh0eyJm4")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("venomXcrazy")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("927996339")
 
 
 
